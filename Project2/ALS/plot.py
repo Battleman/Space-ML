be2a87e2ffe75d037a49cdaef4457c186d8de7e0
Olivier Cloux <olivier.cloux@epfl.ch>
2019-12-10 15:48:55 +0100

    moved plot to ALS

--- Project2/ALS/plot.py
+++ Project2/ALS/plot.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from plotly.graph_objs import Scene, XAxis, YAxis, ZAxis, Layout
 
 
 def plot_raw_data(ratings):
@@ -33,13 +34,14 @@ def plot_raw_data(ratings):
     plt.show()
     # plt.close()
     return num_items_per_user, num_users_per_item
-   
+
+
 def plot_train_test_data(train, test):
     """visualize the train and test data."""
     x_lim = max(train.shape[1], test.shape[1])
     y_lim = max(train.shape[0], test.shape[0])
     print(x_lim, y_lim)
-    fig = plt.figure(figsize=(10,12))
+    fig = plt.figure(figsize=(10, 12))
     ax1 = fig.add_subplot(1, 2, 1)
     ax1.spy(train, precision=0.01, markersize=0.08)
     ax1.set_xlabel("Users")
@@ -60,3 +62,35 @@ def plot_train_test_data(train, test):
     plt.savefig("train_test")
     plt.show()
 
+
+def plot_opti_lambdas(costs):
+    ucosts, icosts = list(zip(*costs))
+    z = list(costs.values())
+    (min_ulambda, min_ilambda,), min_cost = min(
+        costs.items(), key=lambda x: x[1])
+    fig = go.Figure(
+        data=[go.Scatter3d(x=ucosts, y=icosts, z=z,
+                           mode='markers',
+                           marker=dict(
+                               size=8,
+                               color=z,  # set color to an array/list of desired values
+                               colorscale='Viridis',   # choose a colorscale
+                               opacity=0.8,
+                           ),
+                           name="Sample"
+                           ), go.Scatter3d(x=[min_ulambda], y=[min_ilambda], z=[min_cost],
+                                           mode="markers",
+                                           ids=["Minimum"],
+                                           marker=dict(
+                               size=8,
+                               color="red",
+                               opacity=0.8,
+                           ),
+            name="Minimum")
+        ],
+        layout=Layout(scene=Scene(
+            xaxis=XAxis(title='User lambda'),
+            yaxis=YAxis(title='Item lambda'),
+            zaxis=ZAxis(title='Cost')
+        )))
+    fig.show()
