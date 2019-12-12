7d436f7782024365e774a8458b3f6e15566fdb33
OSqalli <othmanesqalli02@gmail.com>
2019-12-12 20:56:59 +0100

    Add NN model

---
+++ Project2/NN/NNmodel.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+import keras
+from keras.models import Model
+from keras import backend as K
+from keras.layers import Concatenate, Dense, Dropout, Flatten
+from keras.layers import Input, Reshape
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.layers import Add, Activation, Lambda
+
+def root_mean_squared_error(y_true, y_pred):
+    """Creates a RMSE loss function for the NN training
+
+    Args:
+        y_true: list of true values of the labels
+        y_pred: list of predicted labels
+
+    """
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+def RecommenderNet(n_users, n_movies, n_factors):
+    """Builds a Neural Network model using the Keras library
+
+    Args:
+        n_users: number of unique users in the dataset
+        n_movies: number of unique movies in the dataset
+        n_factors: number of embeddings (size of the vectors) representing users/movies in the NN model
+
+    """
+    user = Input(shape=(1,))
+    u = Embedding(n_users, n_factors)(user)
+    u = Flatten()(u)
+    u = Dropout(0.02)(u)
+
+    movie = Input(shape=(1,))
+    m = Embedding(n_movies, n_factors)(movie)
+    m = Flatten()(m)
+    m = Dropout(0.02)(m)
+
+    x = keras.layers.concatenate([m, u], axis=1)
+
+    x = Dropout(0.03)(x)
+
+    x = Dense(100, kernel_initializer='he_normal')(x)
+    x = Activation('relu')(x)
+
+    x = Dense(50, kernel_initializer='he_normal')(x)
+    x = Activation('relu')(x)
+
+    x = Dense(10, kernel_initializer='he_normal')(x)
+    x = Activation('relu')(x)
+
+    x = Dense(1, activation='relu', name='Activation')(x)
+    model = Model(inputs=[user, movie], outputs=x)
+    opt = Adam(lr=0.001)
+    model.compile(loss=root_mean_squared_error, optimizer=opt)
+    return model
---
+++ Project2/NN/processing.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+
+import keras
+from keras.models import Model
+
+def preprocessing(path):
+    """ Loads dataset from path and preprocesses it for NN training.
+
+    This also splits the data into a training set and a testing set
+    to perform a cross validation during the NN training.
+
+    Args:
+        path: Path to the data
+
+    """
+    # Loading the training data
+    data = pd.read_csv(path)
+    # Transform data into 2 vectors UserID and MovieId
+    data['userId'] = data['Id'].apply(lambda x: x.split('_')[0][1:]).astype('int')
+    data['movieId'] = data['Id'].apply(lambda x: x.split('_')[1][1:]).astype('int')
+    data.drop('Id', axis=1, inplace=True)
+
+    # Calculate the number of unique users and movies in the dataset
+    n_users = data['userId'].nunique()
+    n_movies = data['movieId'].nunique()
+
+    # Getting the desired input for the NN
+    X = data[['userId', 'movieId']].values
+    y = data['Prediction'].values
+
+    # Splits data into 90% training and 10% testing set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+    X_train_array = [X_train[:, 0], X_train[:, 1]]
+    X_test_array = [X_test[:, 0], X_test[:, 1]]
+
+    return X_train_array, y_train, X_test_array, y_test, n_users, n_movies
+
+def get_uniquevalues(data):
+    """ Calculates the number of unique users and movies in the dataset
+
+    Args:
+        data: Dataframe of original data transformed into 2 user/movie columns
+
+    """
+    n_users = data['userId'].nunique()
+    n_movies = data['movieId'].nunique()
+
+    return n_users, n_movies
+
+def predict(model, path):
+    """Returns predictions of the trained NN model.
+
+    Loads the data given to predict from `path`, runs the trained NN `model`
+    on this data and finally output the ratings predictions.
+
+    Args:
+        model: The trained NN model
+        path: Path to the data given for prediction
+        
+    Returns:
+        list -- List of predictions, tuples of format ("id", rating)
+    """
+    # Loading the sample submission data
+    sample_sumbission = pd.read_csv(path)
+    # Transforming data to have the correct format for the NN
+    sample_sumbission['userId'] = sample_sumbission['Id'].apply(lambda x: x.split('_')[0][1:]).astype('int')
+    sample_sumbission['movieId'] = sample_sumbission['Id'].apply(lambda x: x.split('_')[1][1:]).astype('int')
+    sample_sumbission.drop('Id', axis=1, inplace=True)
+
+    # Get the desired data for prediction
+    X_p = sample_sumbission[['userId', 'movieId']].values
+    X_pred = [X_p[:, 0], X_p[:, 1]]
+
+    # Predict the ratings using the trained NN
+    predictions = model.predict(X_pred)
+    # round predictions
+    rounded = [round(x[0]) for x in predictions]
+
+    # Transform the data into the desired format
+    sample_sumbission['Prediction'] = rounded
+    sample_sumbission['Id'] = 'r' + sample_sumbission['userId'].astype('str') + '_c' + sample_sumbission[
+        'movieId'].astype('str')
+    sample_sumbission.drop(columns=["userId", "movieId"], inplace=True)
+    columnsTitles = ["Id", "Prediction"]
+    sample_sumbission = sample_sumbission.reindex(columns=columnsTitles)
+    # Safety check
+    sample_sumbission = sample_sumbission.replace(6, 5)
+
+    return sample_sumbission
