cea65b6278ebc4b3458548f60c266f203cfb3a2a
Olivier Cloux <olivier.cloux@epfl.ch>
2019-12-16 21:45:50 +0100

    made import proof to any use

--- Project2/Baseline/__init__.py
+++ Project2/Baseline/__init__.py
@@ -1,1 +1,12 @@
-from .main import main
+"""
+Baseline
+=====
+
+Provides an easy way to compute baseline with mean and user/item bias."""
+# -*- coding: utf-8 -*-
+# File ALS/main.py
+
+try:
+    from .main import main
+except (ModuleNotFoundError, ImportError):
+    from main import main
